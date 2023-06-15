17777c47dab8d319b2a9b6f9f82c2799d86e15e7
VladimirS71 <118132491+VladimirS71@users.noreply.github.com>
2023-06-15 17:55:23 +0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -5,18 +5,19 @@ from vk_api.utils import get_random_id
 
 from config import comunity_token, acces_token
 from backend import VkTools
-from data_base import create_db, VK_Data_Base
+from data_store import VK_Data_Base
 # отправка сообщения
 
 
 
 class BotInterface():
     vk_tools = False
+
     def __init__(self, comunity_token, acces_token):
         self.vk = vk_api.VkApi(token=comunity_token)
         self.longpoll = VkLongPoll(self.vk)
         self.vk_tools = VkTools(acces_token)
-        self.params = {}
+        self.params = None
         self.worksheets = []
         self.offset = 0
         self.data = VK_Data_Base()
@@ -36,30 +37,10 @@ class BotInterface():
         #если вернулся фолс работаем с ней дальше
         if not self.data.viewed_id(id(['id']), user_id):
             # делаем запрос в бэк боту - он у себя уже обрабатывает и возвращает ссылку на фотки
-            # получаем фото 
+            # получаем фото
             return self.vk_tools.get_photos(user_id)
         return self.get_worksheet()
     
-    def _get_profiles(self, users: list):
-            """Функция генератор
-            Запрашивает список анкет с указанными параметрами
-            анкета сверяется с БД, если записи по id анкеты нет - 
-            добавляем в БД анкету как просмотренную 
-            :profiles: список id которые вернул запрос поиска
-            :yield: словарь:
-                'id': 123,
-                'f': Полное имя анкеты"""
-            while True:
-                if users:
-                    user = users.pop()
-                    if not self.data.viewed_id(user_id=self.params['id'], worksheet_id=user['id']):
-                        self.data.add_user(user_id=self.params['id'], worksheet_id=user['id'])
-                        yield user
-                else:
-                    users = self.vk_tools.search_worksheet(params=self.params)
-                    if not users:
-                        yield None
-
 # обработка событий / получение сообщений
 
     def event_handler(self):
@@ -83,14 +64,33 @@ class BotInterface():
                             self.offset += 10
                         worksheet = self.worksheets.pop()
                         attach = self.get_worksheet(worksheet['id'])
-                        # если анкета прошла здесь уже фотки 
-                        if attach: 
+                        photo_string = ''
+                        for photo in attach:
+                            photo_string += f'photo{photo["owner_id"]}_{photo["id"]},'
+                        # если анкета прошла здесь уже фото
+                        if attach:
+                            if not self.data.viewed_id(user_id=event.user_id, worksheet_id=worksheet['id']):
+                                self.data.add_user(user_id=event.user_id, worksheet_id=worksheet['id']) 
                             break
                     self.message_send(
                                 event.user_id,
-                                f'имя: {worksheet["name"]} ссылка: vk.com/{worksheet["id"]}',attachment = attach
-                            )
- 
+                                f'имя: {worksheet["name"]} ссылка: vk.com/{worksheet["id"]}', attachment = photo_string
+                            ) 
+                
+                elif event.text.lower() == 'в список избранных':
+                    self.data.update_user(user_id = event.user_id, worksheet_id = worksheet["id"])   
+                    self.message_send(
+                        event.user_id, f'Анкета: {worksheet["name"]} добавлена в избранное')
+                
+                elif event.text.lower() == 'избранное':
+                    favor = self.data.viewed_favorite(user_id = event.user_id)
+                    self.message_send(event.user_id, f'Найдено анкет: {len(favor)}')
+
+                elif event.text.lower() == 'удалить':
+                    self.data.update_user(user_id = event.user_id, worksheet_id = worksheet["id"], favourites= False)   
+                    self.message_send(
+                        event.user_id, f'Анкета: vk.com/id{worksheet["id"]} удалена из избранного')
+
                 elif event.text.lower() == 'пока':
                     self.message_send(
                         event.user_id, 'До новых встреч')
@@ -101,7 +101,10 @@ class BotInterface():
 
 if __name__ == '__main__':
     bot_interface = BotInterface(comunity_token, acces_token)
-    #bot_interface.event_handler()
+    bot_interface.event_handler()
     eng = BotInterface(comunity_token, acces_token)
     attach = eng.get_worksheet(311363878)
+    photo_string = ''
+    for photo in attach:
+        photo_string += f'photo{photo["owner_id"]}_{photo["id"]},'
     print(attach)
